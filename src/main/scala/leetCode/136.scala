50831cae0a19f17b4dee617b0a575c0a9ac78671
Nozomi <nz.yonezawa@gmail.com>
2020-07-25 13:35:26 +0900

    add leetCode files, remove unused file

---
+++ src/main/scala/leetCode/136.scala
@@ -0,0 +1,19 @@
+/* LeetCode 136 
+ * Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+ *
+ * https://leetcode.com/problems/single-number/
+ */
+
+object solution {
+  def main(args: Array[String]): Unit = {
+    val intArray = Array(2, 2, 1)
+    println(singleNumber(intArray))
+
+  }
+  
+  def singleNumber(nums: Array[Int]) = {
+    val nums2: Array[Int] =  nums.distinct  //Array(2, 1)
+    //nums2とnumsで値が2個以下のものだけの配列を作成
+    nums2.filter(n => nums.filter(_ == n).length == 1)(0)
+  }
+}
